cdd833346c75932ec5a6e435314849280f3f8b31
Mahdiur Rahman <rmahdiur@gmail.com>
2019-09-28 19:52:25 -0400

    edede

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login'
+import {Route, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
+
   return (
     <div>
-      <Login />
+      <Router>
+        <div>
+          <Route exact path="/" component={() => <h1>Hello</h1>} />
+          <Route exact path="/login" component={() => <Login></Login>} />
+        </div>
+      </Router> 
     </div>
   );
 }
